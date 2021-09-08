a7d9d7f2ef8d0e27896091b0f9a3fc663bde97a7
Datn7 <datunashvili72@gmail.com>
2021-09-08 05:21:44 +0400

    LINQ

---
+++ LINQ.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOOP
+{
+    class LINQ
+    {
+        static void MyMethod()
+        {
+            var filteredList = new List<int>();
+            var list = new List<int>
+            {
+                1,
+                2,
+                3
+            };
+
+            //use from, where, select, asc, desc etc.
+        }
+    }
+}
